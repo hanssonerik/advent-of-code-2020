b7c25d0877ac4989a5b970e8965a677afc3ff064
erhax <hansson_erik@outlook.com>
2020-12-01 00:12:19 +0100

    refact: destruct fs imports

--- src/hello/hello.ts
+++ src/hello/hello.ts
@@ -1,8 +1,9 @@
 import { promises } from "fs";
 
+const { readFile, writeFile } = promises;
 export const hello = async (value: number) => {
-  const result = await promises.readFile(__dirname + "/input.txt", "utf8");
-  await promises.writeFile(__dirname + "/output.txt", "this is some output");
+  const result = await readFile(__dirname + "/input.txt", "utf8");
+  await writeFile(__dirname + "/output.txt", "this is some output");
   console.debug(result);
   return value * 2;
 };
