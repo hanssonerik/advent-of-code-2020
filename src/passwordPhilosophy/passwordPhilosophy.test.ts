23c43125200f354d5d1af2d7a41aa0e582aef620
erhax <hansson_erik@outlook.com>
2020-12-03 00:35:44 +0100

    feature: PasswordPhilosophy complete

--- src/passwordPhilosophy/passwordPhilosophy.test.ts
+++ src/passwordPhilosophy/passwordPhilosophy.test.ts
@@ -24,7 +24,13 @@
 
 import sut from './passwordPhilosophy'
 
-const { mapToListOfPasswordRecords } = sut
+const {
+  mapToListOfPasswordRecords,
+  isValidPassword,
+  numberOfValidPasswords,
+  getNumberOfValidPasswords,
+  getNumberOfValidPasswordsNewPolicy,
+} = sut
 
 describe('Password Philosophy', () => {
   it('should parse input to list of password records', () => {
@@ -44,5 +50,79 @@ describe('Password Philosophy', () => {
     const passwordRecords = mapToListOfPasswordRecords(inputString)
 
     expect(passwordRecords).toEqual(expected)
+  })
+
+  it('should validate invalid passwordRecord', () => {
+    const passwordRecord = {
+      policy: { atLeast: 1, atMoast: 3, pattern: 'b' },
+      password: 'cdefg',
+    }
+    const expected = false
+
+    const result = isValidPassword(passwordRecord)
+
+    expect(result).toBe(expected)
+  })
+
+  it('should validate valid passwordRecord', () => {
+    const passwordRecord = {
+      policy: { atLeast: 1, atMoast: 3, pattern: 'a' },
+      password: 'abcde',
+    }
+    const expected = true
+
+    const result = isValidPassword(passwordRecord)
+
+    expect(result).toBe(expected)
+  })
+
+  it('should validate invalid passwordRecord', () => {
+    const passwordRecord = {
+      policy: { atLeast: 2, atMoast: 9, pattern: 'c' },
+      password: 'ccccaccbcccccccc',
+    }
+    const expected = false
+
+    const result = isValidPassword(passwordRecord)
+
+    expect(result).toBe(expected)
+  })
+
+  it('should return number of valid passwordRecords', () => {
+    const passwordRecords = [
+      { policy: { atLeast: 1, atMoast: 3, pattern: 'a' }, password: 'abcde' },
+      { policy: { atLeast: 1, atMoast: 3, pattern: 'b' }, password: 'cdefg' },
+      {
+        policy: { atLeast: 2, atMoast: 9, pattern: 'c' },
+        password: 'ccccccccc',
+      },
+    ]
+    const expected = 2
+
+    const result = numberOfValidPasswords(passwordRecords, isValidPassword)
+
+    expect(result).toBe(expected)
+  })
+
+  it('should return number of valid passwordRecords from input file', async () => {
+    const fileName = 'day02-input.in'
+
+    const reportResult = await getNumberOfValidPasswords(
+      `${__dirname}/${fileName}`
+    )
+    console.debug('PasswordPhilosophy valid passwords result: ', reportResult)
+
+    expect(reportResult).toBeDefined()
+  })
+
+  it('should return number of valid passwordRecords from input file with new password policy', async () => {
+    const fileName = 'day02-input.in'
+
+    const reportResult = await getNumberOfValidPasswordsNewPolicy(
+      `${__dirname}/${fileName}`
+    )
+    console.debug('PasswordPhilosophy valid passwords result: ', reportResult)
+
+    expect(reportResult).toBeDefined()
   })
 })
--- src/passwordPhilosophy/passwordPhilosophy.ts
+++ src/passwordPhilosophy/passwordPhilosophy.ts
@@ -1,3 +1,7 @@
+import { promises } from 'fs'
+
+const { readFile } = promises
+
 export const mapToListOfPasswordRecords = (input: string): PasswordRecord[] => {
   return input
     .split('\n')
@@ -19,8 +23,59 @@ export const mapToListOfPasswordRecords = (input: string): PasswordRecord[] => {
     })
 }
 
+export const isValidPassword = (passwordRecord: PasswordRecord): boolean => {
+  const { pattern, atLeast, atMoast } = passwordRecord.policy
+  const result = [
+    ...passwordRecord.password.matchAll(
+      new RegExp(String.raw`${pattern}`, 'g')
+    ),
+  ]
+
+  return result.length <= atMoast && result.length >= atLeast
+}
+
+export const isValidNewPassword = (passwordRecord: PasswordRecord): boolean => {
+  const { pattern, atLeast, atMoast } = passwordRecord.policy
+
+  const first = passwordRecord.password[atLeast - 1] === pattern
+  const second = passwordRecord.password[atMoast - 1] === pattern
+  return (first && !second) || (!first && second)
+}
+
+export const numberOfValidPasswords = (
+  passwordRecords: PasswordRecord[],
+  validator: (passwordRecord: PasswordRecord) => boolean
+) => passwordRecords.filter(p => validator(p)).length
+
+export const getNumberOfValidPasswords = async (
+  path: string
+): Promise<number> => {
+  const passwordRecordsString = await readFile(path, 'utf8')
+  return numberOfValidPasswords(
+    mapToListOfPasswordRecords(passwordRecordsString),
+    isValidPassword
+  )
+}
+
+export const getNumberOfValidPasswordsNewPolicy = async (
+  path: string
+): Promise<number> => {
+  const passwordRecordsString = await readFile(path, 'utf8')
+  return numberOfValidPasswords(
+    mapToListOfPasswordRecords(passwordRecordsString),
+    isValidNewPassword
+  )
+}
+
 interface PasswordRecord {
   policy: { atLeast: number; atMoast: number; pattern: string }
   password: string
 }
-export default { mapToListOfPasswordRecords }
+export default {
+  mapToListOfPasswordRecords,
+  isValidPassword,
+  numberOfValidPasswords,
+  getNumberOfValidPasswords,
+  getNumberOfValidPasswordsNewPolicy,
+  isValidNewPassword,
+}
